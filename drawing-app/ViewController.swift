73c71c13ae34ef289a376c42d8ce35d9e60e5eba
Bisma S Wasesasegara <bisma.s@pegipegi.com>
2020-07-07 09:20:03 +0700

    Project initiation

---
+++ drawing-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  drawing-app
+//
+//  Created by Bisma S Wasesasegara on 07/07/20.
+//  Copyright © 2020 Bisma S Wasesasegara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
